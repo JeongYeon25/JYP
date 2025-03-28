82ddbc0488f235b3a05d51b9088dd05efb575fab
JeongYeon25 <pwjddus5@gmail.com>
2025-03-28 15:50:01 +0900

    Delete hw04_1 directory

--- hw04_1/hw04_1_gugudan.py
+++
@@ -1,4 +0,0 @@
-dan = int(input("몇 단을 출력할까요? =>"))
-for i in range(9):
-    n = i+1
-    print(f"{dan} * {n} = {dan*(n)}")
--- hw04_1/hw04_1_triangle.py
+++
@@ -1,4 +0,0 @@
-n = int(input("몇 줄 짜리 별을 만들까요? =>"))
-for i in range(n):
-    print("*" * (i+1))
-
